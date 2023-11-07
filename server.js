426a9969bc575bd1e212ce23bcac44a6aecdc300
Sebastian4090 <bastek4090@gmail.com>
2023-11-07 11:43:44 +0100

    Add routes and models

--- server.js
+++ server.js
@@ -1,16 +1,15 @@
 const express = require('express');
 const bcrypt = require('bcrypt');
-const { MongoClient } = require('mongodb');
+const mongoose = require('mongoose');
 const cors = require('cors');
 
 
-const user_data = require('./user_data.js'); // To be replaced when deployed
-const signup = require('./controllers/signup.js');
-const profile = require('./controllers/profile.js');
+const user_data = require('./user_data'); // To be replaced when deployed
+const signup = require('./Routes/RegisterRoutes.js');
+const profile = require('./Routes/ProfileRoutes.js');
 
 //Cannot be accessed without user_data file
 const CONNECTION_URL = `mongodb+srv://${user_data.username}:${user_data.password}@fashion-store-db.ars8qan.mongodb.net/?retryWrites=true&w=majority`; 
-const CLIENT = new MongoClient(CONNECTION_URL);
 
 const PORT = process.env.PORT || 3000;
 
@@ -18,13 +17,9 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-app.get('/', (req, res) => {
-    res.send('success');
-})
+mongoose.connect(CONNECTION_URL)
+    .then(() => app.listen(PORT, () => console.log(`Server running on port: ${PORT}`)))
+    .catch((err) => console.log(err.message));
 
 app.post('/sign-up', (req, res) => { signup.handleSignUp(req, res, bcrypt) })
 app.get('/profile/:id', (req, res) => { profile.handleProfileGet(req, res)})
-
-CLIENT.connect()
-.then(() => app.listen(PORT, () => console.log(`Server running on port: ${PORT}`)))
-.catch((err) => console.log(err));
